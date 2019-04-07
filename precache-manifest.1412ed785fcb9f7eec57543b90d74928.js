aaeb2e407b17e9dec9dadc7ea967abefd4cc2f45
Faraks <faraks01@gmail.com>
2019-04-07 21:41:00 +0300

    Updates

---
+++ precache-manifest.1412ed785fcb9f7eec57543b90d74928.js
@@ -0,0 +1,230 @@
+self.__precacheManifest = [
+  {
+    "revision": "fe1eacfd903ffc4bcfc56fa9a39be51e",
+    "url": "/onpoint-test-task/static/media/ice-shard-4.fe1eacfd.png"
+  },
+  {
+    "revision": "9b817a4fe4b52462515e",
+    "url": "/onpoint-test-task/static/css/main.19f0885b.chunk.css"
+  },
+  {
+    "revision": "807964d0d17510f65abc",
+    "url": "/onpoint-test-task/static/js/runtime~main.60553642.js"
+  },
+  {
+    "revision": "24d5bf75d7e43cb27ffd",
+    "url": "/onpoint-test-task/static/js/2.21e31705.chunk.js"
+  },
+  {
+    "revision": "e4618a5b4ea445f71ae3d10981652b0c",
+    "url": "/onpoint-test-task/static/media/musle.e4618a5b.png"
+  },
+  {
+    "revision": "d742f601d4be9bb20631c6026c1bad75",
+    "url": "/onpoint-test-task/static/media/ice-shard-2.d742f601.png"
+  },
+  {
+    "revision": "392beb22030a627999eafc5680342854",
+    "url": "/onpoint-test-task/static/media/ice-shard-3.392beb22.png"
+  },
+  {
+    "revision": "9d4b9e146db5fcd9125a91199a7cdcae",
+    "url": "/onpoint-test-task/static/media/fat-cell.9d4b9e14.png"
+  },
+  {
+    "revision": "1f1afa8a317120fd78357283fb243a70",
+    "url": "/onpoint-test-task/static/media/brain.1f1afa8a.png"
+  },
+  {
+    "revision": "263a1d27cd78bc93910b2b88085b2e81",
+    "url": "/onpoint-test-task/static/media/kidney.263a1d27.png"
+  },
+  {
+    "revision": "a3b85fbb6576dc1fc8e2dad3da009af7",
+    "url": "/onpoint-test-task/static/media/background.a3b85fbb.jpg"
+  },
+  {
+    "revision": "b52db8818d384c7001d04a3cef7f671c",
+    "url": "/onpoint-test-task/static/media/next.b52db881.png"
+  },
+  {
+    "revision": "ea72beb81a68e58526976a8bcb613b6e",
+    "url": "/onpoint-test-task/static/media/1-circle-large.ea72beb8.svg"
+  },
+  {
+    "revision": "cf5bc3f2eb2bcaf56bca6b57818b9a65",
+    "url": "/onpoint-test-task/static/media/1-circle-middle_1.cf5bc3f2.svg"
+  },
+  {
+    "revision": "1bbf58e573db952151bb442900efc0a5",
+    "url": "/onpoint-test-task/static/media/1-circle-middle_2.1bbf58e5.svg"
+  },
+  {
+    "revision": "86c33d79d7f95c9894187b2c0ec9558d",
+    "url": "/onpoint-test-task/static/media/1-circle-small_1.86c33d79.svg"
+  },
+  {
+    "revision": "ba9204b19ca69f84a3a4ea3382ad0cd8",
+    "url": "/onpoint-test-task/static/media/2-circle-large.ba9204b1.svg"
+  },
+  {
+    "revision": "0e9b158f7e8c8c55a029c4c6b2b64538",
+    "url": "/onpoint-test-task/static/media/2-circle-middle.0e9b158f.svg"
+  },
+  {
+    "revision": "057136fe98a42e98d6cd9d6ebde0f87f",
+    "url": "/onpoint-test-task/static/media/2-circle-small_1.057136fe.svg"
+  },
+  {
+    "revision": "960a6c8374fda912d3ff40319739c348",
+    "url": "/onpoint-test-task/static/media/3-circle-large.960a6c83.svg"
+  },
+  {
+    "revision": "d59c677813c7599cf8fedd322ef66e7c",
+    "url": "/onpoint-test-task/static/media/3-circle-middle.d59c6778.svg"
+  },
+  {
+    "revision": "32688283129bc60bb7f544613a6c7ecc",
+    "url": "/onpoint-test-task/static/media/3-circle-small_1.32688283.svg"
+  },
+  {
+    "revision": "f21e306c0b3543baffd3effef71b1693",
+    "url": "/onpoint-test-task/static/media/4-circle-large.f21e306c.svg"
+  },
+  {
+    "revision": "bd0a028110629c267b8bb83aa7514766",
+    "url": "/onpoint-test-task/static/media/4-circle-middle.bd0a0281.svg"
+  },
+  {
+    "revision": "0466bb78a3771de0292fa2d9cab5f6f7",
+    "url": "/onpoint-test-task/static/media/4-circle-small_1.0466bb78.svg"
+  },
+  {
+    "revision": "8f4634cc613fb23b5e4c2ee19699c815",
+    "url": "/onpoint-test-task/static/media/background.8f4634cc.jpg"
+  },
+  {
+    "revision": "ec56bf99fe3260c2831b6ab39daf89c9",
+    "url": "/onpoint-test-task/static/media/ice-shard-1.ec56bf99.png"
+  },
+  {
+    "revision": "e26e0e549d77f02d2e994fba3ad8bf9e",
+    "url": "/onpoint-test-task/static/media/ice-shard-3.e26e0e54.png"
+  },
+  {
+    "revision": "9b817a4fe4b52462515e",
+    "url": "/onpoint-test-task/static/js/main.61777b97.chunk.js"
+  },
+  {
+    "revision": "c89f5b2f247d26cdf54886f3e424c02d",
+    "url": "/onpoint-test-task/static/media/background.c89f5b2f.jpg"
+  },
+  {
+    "revision": "fd78ec0b0f4479a9eb1fd0cc1e9249bb",
+    "url": "/onpoint-test-task/static/media/schema-arrows-1.fd78ec0b.png"
+  },
+  {
+    "revision": "b50b0e3f2c607e32b3c656c69a1ef426",
+    "url": "/onpoint-test-task/static/media/schema-arrows-3.b50b0e3f.png"
+  },
+  {
+    "revision": "279dcf30aee359d989ab00e31dacd7dd",
+    "url": "/onpoint-test-task/static/media/intestines.279dcf30.png"
+  },
+  {
+    "revision": "78660e37614b214b4813cf9cf2f7f8d2",
+    "url": "/onpoint-test-task/static/media/infection.78660e37.png"
+  },
+  {
+    "revision": "ecdbfe720e15507b720bf14aed8b69ca",
+    "url": "/onpoint-test-task/static/media/lining.ecdbfe72.png"
+  },
+  {
+    "revision": "c31a8e097cb6c2901857d8c686f0063d",
+    "url": "/onpoint-test-task/static/media/GothamPro.c31a8e09.eot"
+  },
+  {
+    "revision": "3cb46b372ab77d032576b9f70c83d1ff",
+    "url": "/onpoint-test-task/static/media/GothamPro.3cb46b37.woff"
+  },
+  {
+    "revision": "3100f91bbd9e9ca9ecd00255cac7d11c",
+    "url": "/onpoint-test-task/static/media/GothamPro.3100f91b.ttf"
+  },
+  {
+    "revision": "677c3a32938f905b22eb14afd1d5cff4",
+    "url": "/onpoint-test-task/static/media/GothamPro-Light.677c3a32.woff"
+  },
+  {
+    "revision": "7fd3861de823f75a6923000a948c494a",
+    "url": "/onpoint-test-task/static/media/GothamPro-Light.7fd3861d.eot"
+  },
+  {
+    "revision": "abd5115e2ddae490f9cb393fb8cdfc5a",
+    "url": "/onpoint-test-task/static/media/GothamPro-Light.abd5115e.ttf"
+  },
+  {
+    "revision": "cd6c6aac15595c65e75760bce456198a",
+    "url": "/onpoint-test-task/static/media/Lato-Regular.cd6c6aac.woff"
+  },
+  {
+    "revision": "3b8c0c7776942ac25543ce1864bfeba4",
+    "url": "/onpoint-test-task/static/media/Lato-Regular.3b8c0c77.eot"
+  },
+  {
+    "revision": "79b8ba6c6b392a2c85891368896f83b1",
+    "url": "/onpoint-test-task/static/media/Lato-Light.79b8ba6c.woff"
+  },
+  {
+    "revision": "fe3d8decb24d2232afefce5a1cd28c73",
+    "url": "/onpoint-test-task/static/media/Lato-Light.fe3d8dec.eot"
+  },
+  {
+    "revision": "02e85e244c78339c433b99a744f54b55",
+    "url": "/onpoint-test-task/static/media/Lato-Regular.02e85e24.ttf"
+  },
+  {
+    "revision": "12ef52cc546567c7549dd2e53025b47f",
+    "url": "/onpoint-test-task/static/media/Lato-Medium.12ef52cc.woff"
+  },
+  {
+    "revision": "32426ae551632ee97541769514abb18c",
+    "url": "/onpoint-test-task/static/media/Lato-Light.32426ae5.ttf"
+  },
+  {
+    "revision": "3ed0b9fefe1d39976dc57ec384aefaac",
+    "url": "/onpoint-test-task/static/media/Lato-Medium.3ed0b9fe.ttf"
+  },
+  {
+    "revision": "1f242eab41f35bb8c5f94078edf9965b",
+    "url": "/onpoint-test-task/static/media/Lato-Medium.1f242eab.eot"
+  },
+  {
+    "revision": "f7a0c31e13069b62fbdf68a957a0fc51",
+    "url": "/onpoint-test-task/static/media/Lato-Bold.f7a0c31e.woff"
+  },
+  {
+    "revision": "c8ef5e00219e7a561cdbb27290fb8781",
+    "url": "/onpoint-test-task/static/media/Lato-Bold.c8ef5e00.eot"
+  },
+  {
+    "revision": "bef8639602379476aaade35f341782b6",
+    "url": "/onpoint-test-task/static/media/Lato-Semibold.bef86396.woff"
+  },
+  {
+    "revision": "cff5cdfe16eab4a8f10a42ccf7d58adb",
+    "url": "/onpoint-test-task/static/media/Lato-Semibold.cff5cdfe.eot"
+  },
+  {
+    "revision": "d6e8a95af622b5f08cbe540f2f56e087",
+    "url": "/onpoint-test-task/static/media/Lato-Bold.d6e8a95a.ttf"
+  },
+  {
+    "revision": "eedc65d00d283922f343d6228632762c",
+    "url": "/onpoint-test-task/static/media/Lato-Semibold.eedc65d0.ttf"
+  },
+  {
+    "revision": "04b3f159a96b165eb389acc27c09bd4c",
+    "url": "/onpoint-test-task/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/onpoint-test-task/precache-manifest.1412ed785fcb9f7eec57543b90d74928.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/onpoint-test-task/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
